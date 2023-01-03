88699844168f692ed2c4f1f450f7cf35fa149435
Lizeth <perlizy.1996@gmail.com>
2023-01-02 18:07:50 -0700

    Consume API & modificate HTML

--- mysite/crypto/views.py
+++ mysite/crypto/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'home.html', {})
+    import pip._vendor.requests 
+    import json
+
+    price_request= pip._vendor.requests.get("https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,USDT,XLM,EOS&tsyms=USD")
+    price= json.loads(price_request.content)
+    
+
+    api_request= pip._vendor.requests.get("https://min-api.cryptocompare.com/data/v2/news/?lang=EN")
+    api= json.loads(api_request.content)
+    return render(request, 'home.html', {'api':api, 'price':price})
+
+
+def prices(request):
+    return render(request, 'prices.html', {})
